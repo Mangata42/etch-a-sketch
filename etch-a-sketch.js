6ba74d27a95c7330f75aa1d28e7cdd0878e9e5b7
Mangata <nghaddar@student.42.fr>
2022-09-22 11:08:36 +0200

    hovering change background color of the divs:

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,14 @@
 let pixelContainer = document.querySelector(".pixel-container");
+
+function onEnter(e)
+{
+    this.style.backgroundColor = "blue";
+}
+
 for (let i = 0; i < 16; i++)
 {
     let pixel = document.createElement("div");
     pixel.classList = "pixel";
     pixelContainer.appendChild(pixel);
+    pixel.addEventListener("mouseenter", onEnter);
 }
